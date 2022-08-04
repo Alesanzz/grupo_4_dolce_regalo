4c179e43e54e11432881b94ba08c7c0a17a72edb
Alesanzz <alejandro240295@hotmail.com>
2022-08-03 22:54:30 -0300

    organizando un poco el app.js, subiendo varias fotos, agregando la pagina responsive del home, asi como todo el sistema de ruteo y su diseño del css

--- source/app.js
+++ source/app.js
@@ -1,31 +1,38 @@
+//requiriendo express basico
 const express = require("express");
-const path = require("path");
-//las mejores
+
+//configuracion de servidor
 const server = express();
-const port = process.env.PORT || 3030;
-const loginRoutes = require('./routes/auth/login.routes');
-const registerRoutes = require('./routes/auth/register.routes');
-server.listen(port, () => {
-    console.log(`Starting server at port ${port}`);
-});
-//config engine ejs
-server.set('view engine', 'ejs');
-//views 
-server.set("views", path.join(__dirname, "views"));
-//static files
-const publicPath = path.resolve(__dirname, "../public");
-server.use(express.static(publicPath));
-//home
-server.get("/", function(req, res) {
-    let file = path.resolve(__dirname, "views", "home.html");
-    res.sendFile(file);
-});
-//path login
-server.use('/', loginRoutes)
-    //path register 
-server.use('/', registerRoutes)
-    // product
-server.get("/product", function(req, res) {
-    let file = path.resolve(__dirname, "views", "products.html");
-    res.sendFile(file);
-});
+const config = require("./modules/server");
+server.listen(config.port, config.start());
+
+//configurando express para el uso de los verbos de html
+server.use(express.urlencoded({ extended: true }));
+
+//configuracion de rutas
+const path = require("path");
+const public = path.join(__dirname, "../public");
+
+//configuracion de static
+const static = require("./modules/static");
+server.use(static(public));
+
+//configuracion de engine ejs
+server.set("views", path.join(__dirname, "./views"));
+server.set("view engine", "ejs");
+
+
+//requiriendo las rutas de las paginas web
+const homeRoutes = require("./routes/home.routes");
+const loginRoutes = require("./routes/auth/login.routes");
+const registerRoutes = require("./routes/auth/register.routes");
+//AQUI REQUERIR LA RUTA DE PRODUCT
+
+
+//path del home
+server.use("/", homeRoutes);
+//path del login
+server.use("/", loginRoutes);
+//path del register
+server.use("/", registerRoutes);
+//path del product
--- source/routes/auth/login.routes.js
+++ source/routes/auth/login.routes.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const loginController = require('../../controllers/auth/login.controller')
-router.get('/login', loginController.get)
+const loginController = require("../../controllers/auth/login.controller");
+
+router.get("/login", loginController.get);
 
 module.exports = router;
