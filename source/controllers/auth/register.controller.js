47c8c6f2529bfe4339c5c66e488ec40f0f8f9d4b
Oliver Charry <oliver.charry.216@unisabaneta.edu.co>
2022-08-11 09:28:03 -0500

    ok

--- source/controllers/auth/register.controller.js
+++ source/controllers/auth/register.controller.js
@@ -5,24 +5,24 @@ const usersModel = require("../../models/auth/users.model");
 
 
 const registerController = {
-    create: function (req, res) {
+    create: function(req, res) {
         return res.render("register");
-      },
+    },
 
-    save: function (req, res)  {
+    save: function(req, res) {
         if (req.body.image && req.files.length > 0) {
             req.body.image = req.files[0].filename
-        }else{
+        } else {
             req.body.image = "default-user-image.png"
         }
-
+        console.log(req.body);
         let nuevo = usersModel.generate(req.body);
         let todos = usersModel.all();
         todos.push(nuevo);
         usersModel.write(todos);
         return res.redirect("/login");
-        }
     }
+}
 
 
 
