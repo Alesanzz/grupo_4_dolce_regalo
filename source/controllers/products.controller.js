1cf4da454f2d51fa31cf47fbce0a06b7f2c1a4f4
Oliver Charry <oliver.charry.216@unisabaneta.edu.co>
2022-08-11 22:24:27 -0500

    ajuste de product detail

--- source/controllers/products.controller.js
+++ source/controllers/products.controller.js
@@ -4,8 +4,19 @@ const productController = {};
 const dataProducts = require('../data/products.json')
 productController.get = (req = request, res = response) => {
     try {
-        console.log(dataProducts);
+        // console.log(dataProducts);
         res.render('product', { data: dataProducts })
+    } catch (error) {
+        res.status(500).send('error de servidor')
+    }
+}
+productController.getById = (req = request, res = response) => {
+    try {
+        let sku = parseInt(req.params.id)
+
+        let productDetail = dataProducts.filter((value) => value.sku === sku);
+        console.log(productDetail, sku);
+        res.render('product-detail', { product: productDetail })
     } catch (error) {
         res.status(500).send('error de servidor')
     }
--- source/routes/products.routes.js
+++ source/routes/products.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const productController = require('../controllers/products.controller')
 
 router.get("/products", productController.get);
+router.get("/products/:id", productController.getById);
 
 module.exports = router;
