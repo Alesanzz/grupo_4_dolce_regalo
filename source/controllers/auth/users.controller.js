22d6b312320a7a5c95d2f82c6c8adf3982080dbb
Oliver Charry <oliver.charry.216@unisabaneta.edu.co>
2022-10-17 22:04:07 -0500

    mejoras opliver

--- source/controllers/auth/users.controller.js
+++ source/controllers/auth/users.controller.js
@@ -4,63 +4,64 @@ const expressValidator = require("express-validator");
 const usersModel = require("../../models/auth/users.model");
 
 const registerController = {
-  create: function (req, res) {
-    return res.render("register");
-  },
+    create: function(req, res) {
+        return res.render("register");
+    },
 
-  save: function (req, res) {
-    // Control de las validaciones
-    const result = expressValidator.validationResult(req);
-    if (!result.isEmpty()) {
-      // El mapped hace mas legible los errores para Java
-      let errores = result.mapped();
-      return res.render("register", {
-        errores: errores,
-        data: req.body,
-      });
-    }
+    save: function(req, res) {
+        // Control de las validaciones
+        const result = expressValidator.validationResult(req);
+        if (!result.isEmpty()) {
+            // El mapped hace mas legible los errores para Java
+            let errores = result.mapped();
+            return res.render("register", {
+                errores: errores,
+                data: req.body,
+            });
+        }
 
-    // Si pasamos las validaciones, ocurre lo siguiente:
-    if (req.body && req.files.length > 0) {
-      req.body.image = req.files[0].filename;
-    } else {
-      req.body.image = "default-user-image.png";
-    }
+        // Si pasamos las validaciones, ocurre lo siguiente:
+        if (req.body && req.files.length > 0) {
+            req.body.image = req.files[0].filename;
+        } else {
+            req.body.image = "default-user-image.png";
+        }
 
-    let nuevo = usersModel.generate(req.body);
-    let todos = usersModel.all();
-    todos.push(nuevo);
-    usersModel.write(todos);
-    return res.redirect("/login");
-  },
+        let nuevo = usersModel.generate(req.body);
+        let todos = usersModel.all();
+        todos.push(nuevo);
+        usersModel.write(todos);
+        return res.redirect("/login");
+    },
 
-  login: function (req, res) {
-    return res.render("login");
-  },
+    login: function(req, res) {
+        return res.render("login");
+    },
 
-  access: function (req, res) {
-    // Control de las validaciones
-    const result = expressValidator.validationResult(req);
-    if (!result.isEmpty()) {
-      // El mapped hace mas legible los errores para Java
-      let errores = result.mapped();
-      return res.render("login", {
-        errores: errores,
-        data: req.body,
-      });
-    }
+    access: function(req, res) {
+        // Control de las validaciones
+        const result = expressValidator.validationResult(req);
+        console.log('MESSI', result);
+        if (!result.isEmpty()) {
+            // El mapped hace mas legible los errores para Java
+            let errores = result.mapped();
+            return res.render("login", {
+                errores: errores,
+                data: req.body,
+            });
+        }
 
-    res.cookie("user", req.body.email, { maxAge: 1000 * 60 * 60 });
-    let all = usersModel.all();
-    req.session.user = all.find((user) => user.email == req.body.email);
-    return res.redirect("/");
-  },
+        res.cookie("user", req.body.email, { maxAge: 1000 * 60 * 60 });
+        let all = usersModel.all();
+        req.session.user = all.find((user) => user.email == req.body.email);
+        return res.redirect("/");
+    },
 
-  logout: function (req, res) {
-    delete req.session.user;
-    res.cookie("user", null, { maxAge: -1 });
-    return res.redirect("/");
-  },
+    logout: function(req, res) {
+        delete req.session.user;
+        res.cookie("user", null, { maxAge: -1 });
+        return res.redirect("/");
+    },
 };
 
 module.exports = registerController;
--- source/controllers/auth/users.controller2.js
+++ source/controllers/auth/users.controller2.js
@@ -5,147 +5,146 @@ const db = require("../../database/models");
 const expressValidator = require("express-validator");
 
 const userController = {
-  create: function (req, res) {
-    return res.render("users-views/register");
-  },
+    create: function(req, res) {
+        return res.render("users-views/register");
+    },
 
-  save: function (req, res) {
-    // Control de las validaciones
-    const result = expressValidator.validationResult(req);
-    if (!result.isEmpty()) {
-      // El mapped hace mas legible los errores para Java
-      let errores = result.mapped();
-      return res.render("users-views/register", {
-        errores: errores,
-        data: req.body,
-      });
-    }
-    // Si pasamos las validaciones, ocurre lo siguiente:
-    if (req.body && req.files.length > 0) {
-      req.body.image = req.files[0].filename;
-    } else {
-      req.body.image = "default-user-image.png";
-    }
+    save: function(req, res) {
+        // Control de las validaciones
+        const result = expressValidator.validationResult(req);
+        if (!result.isEmpty()) {
+            // El mapped hace mas legible los errores para Java
+            let errores = result.mapped();
+            return res.render("users-views/register", {
+                errores: errores,
+                data: req.body,
+            });
+        }
+        // Si pasamos las validaciones, ocurre lo siguiente:
+        if (req.body && req.files.length > 0) {
+            req.body.image = req.files[0].filename;
+        } else {
+            req.body.image = "default-user-image.png";
+        }
 
-    const save = db.User.create({
-      name: req.body.name,
-      lastname: req.body.lastname,
-      phone: Number(req.body.phone),
-      country: req.body.country,
-      email: req.body.email,
-      // Para encriptar una contraseña, se debe utilizar "bcrypt.hashSync"
-      password: bcrypt.hashSync(req.body.password, 10),
+        const save = db.User.create({
+            name: req.body.name,
+            lastname: req.body.lastname,
+            phone: Number(req.body.phone),
+            country: req.body.country,
+            email: req.body.email,
+            // Para encriptar una contraseña, se debe utilizar "bcrypt.hashSync"
+            password: bcrypt.hashSync(req.body.password, 10),
 
-      avatar: req.body.image,
-    });
-    const success = (data) => res.redirect("/login");
-    const error = (error) => res.send(error);
+            avatar: req.body.image,
+        });
+        const success = (data) => res.redirect("/login");
+        const error = (error) => res.send(error);
 
-    return save.then(success).catch(error);
-  },
+        return save.then(success).catch(error);
+    },
 
-  login: function (req, res) {
-    return res.render("users-views/login");
-  },
+    login: function(req, res) {
+        return res.render("users-views/login");
+    },
 
-  access: function (req, res) {
-    // Control de las validaciones
-    const result = expressValidator.validationResult(req);
-    if (!result.isEmpty()) {
-      // El mapped hace mas legible los errores para Java
-      let errores = result.mapped();
-      return res.render("users-views/login", {
-        errores: errores,
-        data: req.body,
-      });
-    }
+    access: function(req, res) {
+        // Control de las validaciones
+        const result = expressValidator.validationResult(req);
+        if (!result.isEmpty()) {
+            // El mapped hace mas legible los errores para Java
+            let errores = result.mapped();
+            return res.render("users-views/login", {
+                errores: errores,
+                data: req.body,
+            });
+        }
+        res.cookie("user", req.body.email, { maxAge: 1000 * 60 * 60 });
+        const userLogged = db.User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
 
-    res.cookie("user", req.body.email, { maxAge: 1000 * 60 * 60 });
-    const userLogged = db.User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
+        req.session.user = userLogged;
+        return res.redirect("/");
+    },
 
-    req.session.user = userLogged;
-    return res.redirect("/");
-  },
+    logout: function(req, res) {
+        delete req.session.user;
+        res.cookie("user", null, { maxAge: -1 });
+        return res.redirect("/");
+    },
 
-  logout: function (req, res) {
-    delete req.session.user;
-    res.cookie("user", null, { maxAge: -1 });
-    return res.redirect("/");
-  },
+    myprofile: function(req, res) {
+        return res.render("users-views/profile");
+    },
 
-  myprofile: function (req, res) {
-    return res.render("users-views/profile");
-  },
+    edit: function(req, res) {
+        const userToEdit = db.User.findByPk(req.params.sku);
+        const success = (usuario) =>
+            res.render("users-views/profile-edit", { usuario });
+        const error = (error) => res.send(error);
 
-  edit: function (req, res) {
-    const userToEdit = db.User.findByPk(req.params.sku);
-    const success = (usuario) =>
-      res.render("users-views/profile-edit", { usuario });
-    const error = (error) => res.send(error);
+        return userToEdit.then(success).catch(error);
+    },
 
-    return userToEdit.then(success).catch(error);
-  },
+    update: async function(req, res) {
+        // Control de las validaciones
+        const result = expressValidator.validationResult(req);
+        if (!result.isEmpty()) {
+            // El mapped hace mas legible los errores para Java
+            let errores = result.mapped();
+            return res.render("users-views/profile-edit", {
+                errores: errores,
+                data: req.body,
+            });
+        }
+        // Si pasamos las validaciones, ocurre lo siguiente:
+        try {
+            const usuarioAEditar = await db.User.findByPk(req.body.sku);
 
-  update: async function (req, res) {
-    // Control de las validaciones
-    const result = expressValidator.validationResult(req);
-    if (!result.isEmpty()) {
-      // El mapped hace mas legible los errores para Java
-      let errores = result.mapped();
-      return res.render("users-views/profile-edit", {
-        errores: errores,
-        data: req.body,
-      });
-    }
-    // Si pasamos las validaciones, ocurre lo siguiente:
-    try {
-      const usuarioAEditar = await db.User.findByPk(req.body.sku);
+            if (req.files && req.files.length > 0) {
+                req.body.avatar = req.files[0].filename;
+            } else {
+                req.body.avatar = usuarioAEditar.image;
+            }
 
-      if (req.files && req.files.length > 0) {
-        req.body.avatar = req.files[0].filename;
-      } else {
-        req.body.avatar = usuarioAEditar.image;
-      }
+            await usuarioAEditar.update({
+                ...req.body,
+                phone: Number(req.body.phone),
+                password: bcrypt.hashSync(req.body.password, 10),
+            });
+            return res.redirect("/profile/:sku");
+        } catch (error) {
+            return res.send(error);
+        }
+    },
 
-      await usuarioAEditar.update({
-        ...req.body,
-        phone: Number(req.body.phone),
-        password: bcrypt.hashSync(req.body.password, 10),
-      });
-      return res.redirect("/profile/:sku");
-    } catch (error) {
-      return res.send(error);
-    }
-  },
-
-  destroy: async function (req, res) {
-    try {
-      let usuarioAEliminar = await db.User.findByPk(req.body.sku);
-      //para que se eliminen las imagenes
-      if (usuarioAEliminar.avatar != "default-user-image.png") {
-        let file = path.resolve(
-          __dirname,
-          "..",
-          "..",
-          "..",
-          "public",
-          "images",
-          "users",
-          usuarioAEliminar.avatar
-        );
-        fs.unlinkSync(file);
-      }
-      //para eliminar los datos
-      const remove = await usuarioAEliminar.destroy();
-      return res.redirect("/");
-    } catch (error) {
-      return res.send(error);
-    }
-  },
+    destroy: async function(req, res) {
+        try {
+            let usuarioAEliminar = await db.User.findByPk(req.body.sku);
+            //para que se eliminen las imagenes
+            if (usuarioAEliminar.avatar != "default-user-image.png") {
+                let file = path.resolve(
+                    __dirname,
+                    "..",
+                    "..",
+                    "..",
+                    "public",
+                    "images",
+                    "users",
+                    usuarioAEliminar.avatar
+                );
+                fs.unlinkSync(file);
+            }
+            //para eliminar los datos
+            const remove = await usuarioAEliminar.destroy();
+            return res.redirect("/");
+        } catch (error) {
+            return res.send(error);
+        }
+    },
 };
 
 module.exports = userController;
