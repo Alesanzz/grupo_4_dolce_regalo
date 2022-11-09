356cd293254775aa8f50106984d37ad3060fa7fd
Oliver Charry <oliver.charry.216@unisabaneta.edu.co>
2022-11-09 09:15:11 -0500

    subiendo ajustes para hacer merge con main

--- backend/source/database/config/config.js
+++ backend/source/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     "development": {
         "username": "root",
         "password": null,
-        "database": "dolce_regalo2",
+        "database": "dolce_regalo",
         "host": "127.0.0.1",
         "dialect": "mysql",
         "port": 3306
